eb9ec71b71afd60e168a169c7d0aefb35662e854
Jin Yu <github@jsy.us>
2024-10-26 13:23:04 -0400

    store urls

---
+++ Sources/KeweApp/AppStoreLinks.swift
@@ -0,0 +1,9 @@
+
+import Foundation
+
+public enum AppStoreLinks {
+    public static let developer = "https://apps.apple.com/us/developer/jin-yu/id1054838879"
+    public static let DataRadar = "https://apps.apple.com/us/app/date-radar-countdown-stopwatch/id6463448697"
+    public static let ChargeRadar = "https://apps.apple.com/us/app/charge-radar-for-tesla-ev/id6451003230"
+    public static let TipCalc = "https://apps.apple.com/us/app/tip-calc-bill-splitter/id1054838880"
+}
